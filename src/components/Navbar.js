4a83c0fd257ec5e64d739494ee50c2d0432adeb1
Adrian Leal <adleal956@gmail.com>
2022-09-09 23:53:48 -0700

    Test

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import { Navbar, NavbarBrand, Collapse, NavbarToggler, Nav, NavItem } from 'reactstrap';
+import { NavLink } from 'react-router-dom';
+
+const MyNavbar = () => {
+	return (
+		<Navbar>
+			<NavbarBrand>ADDDDD</NavbarBrand>
+		</Navbar>
+	);
+};
+
+export default MyNavbar;
