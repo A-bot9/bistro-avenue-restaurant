decabe89a0c6384d2a8134f2be7f3bf1d01bc66d
Adrian Leal <adleal956@gmail.com>
2022-09-16 23:09:34 -0700

    finished ChooseUs component

--- src/components/choose-us/ChooseUs.jsx
+++ src/components/choose-us/ChooseUs.jsx
@@ -16,10 +16,11 @@ const ChooseUs = () => {
 						<div className="choose__content">
 							<h4>Who we are?</h4>
 							<h2>Take a look at the benefits we offer you</h2>
+							<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
 						</div>
 
 						<div className="features mt-4">
-							<div className="feature1 d-flex align-items-center justify-content-between">
+							<div className="feature1 d-flex align-items-center justify-content-between gap-5">
 								<div className="single__feature">
 									<span>
 										<i className="ri-truck-line"></i>
@@ -33,6 +34,24 @@ const ChooseUs = () => {
 										<i className="ri-money-dollar-circle-line"></i>
 									</span>
 									<h6>Return & Refund</h6>
+									<p>Lorem ipsum dolor sit amet.</p>
+								</div>
+							</div>
+
+							<div className="feature1 mt-3 d-flex align-items-center justify-content-between gap-5">
+								<div className="single__feature">
+									<span className="feature__icon-3">
+										<i className="ri-secure-payment-line"></i>
+									</span>
+									<h6>Secure Payment</h6>
+									<p>Lorem ipsum dolor sit amet.</p>
+								</div>
+
+								<div className="single__feature">
+									<span className="feature__icon-4">
+										<i className="ri-24-hours-line"></i>
+									</span>
+									<h6>24/7 Hours Support</h6>
 									<p>Lorem ipsum dolor sit amet.</p>
 								</div>
 							</div>
