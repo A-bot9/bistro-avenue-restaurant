91f360907d6697231eaacab5ce4cc0f6cbc36572
Adrian Leal <adleal956@gmail.com>
2022-09-16 22:44:47 -0700

    ProductMenu, ProductCard and part of ChooseUs

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,16 @@
 import React, { Fragment } from 'react';
+import ChooseUs from '../components/choose-us/ChooseUs';
 import Header from '../components/header/Header';
 import HeroSlider from '../components/hero-slider/HeroSlider';
+import PopularMenu from '../components/popular-menu/PopularMenu';
 
 const Home = () => {
 	return (
 		<Fragment>
 			<Header />
 			<HeroSlider />
+			<PopularMenu />
+			<ChooseUs />
 		</Fragment>
 	);
 };
